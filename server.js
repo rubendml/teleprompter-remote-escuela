119e78bccee0388bcc96f2cddaf51a778c3c8543
Ruben Dario Monroy León <rubendml@gmail.com>
2026-01-22 19:41:46 -0500

    Primera versión del teleprompter

---
+++ server.js
@@ -0,0 +1,16 @@
+const WebSocket = require("ws");
+const PORT = process.env.PORT || 3000;
+
+const wss = new WebSocket.Server({ port: PORT });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+});
+
+console.log("🟢 WebSocket activo en puerto", PORT);
